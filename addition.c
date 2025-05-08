8dc62c8f76af26e93974a6c166d9c3d60c21f8d1
apy0108 <aniketyerawar182003@gmail.com>
2025-05-08 23:13:18 +0530

    Update addition.c

--- addition.c
+++ addition.c
@@ -1,19 +1,21 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <pic18f4550.h>
+#include<stdio.h>
+
+#include<stdlib.h>
+
+#include<p18f4550.h>
 
 void main(void)
+
 {
-    unsigned char num1, num2, sum;
 
-    num1 = 0xA0;
-    num2 = 0x02;
-    sum = num1 + num2;
+int i, sum,i;
 
-    TRISD = 0; 
+sum=0;
 
-    while (1)
-    {
-      PORTD = sum; 
-    }
+sum=0x0A+0x03;
+
+TRISD=0;
+
+PORTD=sum;
+
 }
