32bf8620a20952347ae36d2d18a26750955d90dd
apy0108 <aniketyerawar182003@gmail.com>
2025-05-08 22:57:25 +0530

    Create rasberi.c

---
+++ rasberi.c
@@ -0,0 +1,16 @@
+import RPi.GPIO as X import time X.setmode(X.BOARD)
+                                               X.setup(3, X.OUT)
+
+                                                              while True:X.output(3, True)
+                                                                                         time.sleep(1)
+                                                                                                        X.output(3, False)
+                                                                                                                        time.sleep(1)
+                                                                                                                            CODE FOR FIXED STATE(ON) : -
+                                                                                                                                                 import RPi.GPIO as X import time X.setmode(X.BOARD)
+                                                                                                                                                                                                X.setup(3, X.OUT)
+                                                                                                                                                                                                               X.output(3, True)
+
+                                                                                                                                                                                                                            CODE FOR FIXED STATE(ON) : -
+                                                                                                                                                                                                                                                 import RPi.GPIO as X import time X.setmode(X.BOARD)
+                                                                                                                                                                                                                                                                                                X.setup(3, X.OUT)
+                                                                                                                                                                                                                                                                                                               X.output(3, False)
