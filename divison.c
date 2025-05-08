41ef3ee6bc335724605b155bec94cd7dd4b77e99
apy0108 <aniketyerawar182003@gmail.com>
2025-05-08 23:14:12 +0530

    Update divison.c

--- divison.c
+++ divison.c
@@ -1,21 +1,21 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <PIC18F4550.h>
+#include<stdio.h>
+
+#include<stdlib.h>
+
+#include<p18f4550.h>
 
 void main(void)
-{
-    int num1, num2;
-    int result;
-    result = 0;
-    num1 = 0x23;
-    num2 = 0x10;
-    
-    while (num1 >= num2)
-    {
-        num1 = num1 - num2;
-        result++;
-    }
 
-    TRISD = 0;
-    PORTD = result; // Assign the value of result to PORTD
-}
+{
+
+int i, sum,i;
+
+sum=0;
+
+sum=0x08/0x02;
+
+TRISD=0;
+
+PORTD=sum;
+
+};
